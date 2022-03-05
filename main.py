8ec1c24ae6a9aacefc023c155f911fb730946ac4
tbjep <tbjepsen1@gmail.com>
2022-03-06 00:47:55 +0100

    Play a sine wave

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
+import numpy as np
 
 samplerate = 44100
-
 duration = 3
 
 print("Recording...")
@@ -15,3 +15,20 @@ print("Recording done. Playing back.")
 sd.play(recording, samplerate)
 
 sd.wait()
+
+print("Playing a (hopefully) smooth sine wave")
+
+class SineGenerator:
+    def __init__(self):
+        self.i = 0
+
+    def callback(self, outdata, frames, time, status):
+        if status:
+            print(status)
+
+        outdata[:] = np.sin(2 * np.pi * 440 * (self.i + np.arange(frames)) / samplerate).reshape(-1, 1)
+        self.i += frames
+
+sg = SineGenerator()
+with sd.OutputStream(samplerate, channels=1, callback=sg.callback):
+    input("Press enter to quit.")
